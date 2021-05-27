12d24044a1d68ef16dcaee4992105450e874f70a
bayuirsad26 <c0070763@bangkit.academy>
2021-05-28 00:28:31 +0700

    change config database

--- src/configs/database.js
+++ src/configs/database.js
@@ -3,5 +3,5 @@ module.exports = {
     host                : '34.101.216.85',
     user                : 'root',
     password            : '',
-    database            : 'b21-cap0192:asia-southeast2:capstone'
+    database            : 'capstone'
 };
