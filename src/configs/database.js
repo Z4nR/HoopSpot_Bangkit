261fc7033a3f069c8ac3be6fff5bd51ae77862be
bayuirsad26 <c0070763@bangkit.academy>
2021-06-02 04:10:44 +0700

    little change in comment section

--- src/configs/database.js
+++ src/configs/database.js
@@ -10,5 +10,5 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`*/
+    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` */
 };
