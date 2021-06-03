87ee5f7928ba95088e47e57dfb065cf62fe9ab5e
bayuirsad26 <c0070763@bangkit.academy>
2021-06-03 07:20:27 +0700

    add file to config deployment at cloud

--- detector in python/main.py
+++ detector in python/main.py
@@ -101,5 +101,4 @@ def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+@app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
